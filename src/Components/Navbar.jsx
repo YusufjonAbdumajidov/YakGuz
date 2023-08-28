d03af24ff1910451199863ff95cb9169b45b54cf
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-08-28 21:24:27 +0300

    working on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,17 @@ const Navbar = () => {
             <span>+99855 404-18-74</span>
             <span>G’uzor shaxri, Mustaqillik ko’chasi 41 uy</span>
         </section>
-        <section></section>
+        <section>
+            <h1>YAKGUZ</h1>
+            <div>
+                <p>Ma'lumot</p>
+                <p>Rahbariyat</p>
+                <p>Qabul</p>
+                <p>...</p>
+                <p>...</p>
+                <p>Aloqa</p>
+            </div>
+        </section>
     </div>
   )
 }
