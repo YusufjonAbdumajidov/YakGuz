17bb809b429a8fcf335409414126960b75c90b6c
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-09-29 10:32:13 +0300

    working on design

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import kadrlar1 from "../assets/kadrlar1.jpg";
+import kadrlar2 from "../assets/kadrlar2.jpg";
 
 const About = () => {
   return (
     <div className='about'>
         <h1>Yosh kadrlar</h1>
+        <img src={kadrlar1} alt="" />
     </div>
   )
 }
