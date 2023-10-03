ff5b2b89497aa3e2c2b6dd7b151deb626129e8c9
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-03 12:28:28 +0300

    placing components

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import Main from "./Main";
 import About from "./About";
+import Hamkorlik from "./Hamkorlik";
+
 
 const Home = () => {
   return <>
-        <Main />
-        <About />
+      <Main />
+      <About />
+      <Hamkorlik />
   </>
 }
 
