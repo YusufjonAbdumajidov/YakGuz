a9614bd95316e7a0fc73085c857b05820ce9145a
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-04 10:49:10 +0300

    new text added

--- src/Components/Hamkorlik.jsx
+++ src/Components/Hamkorlik.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Hamkorlik = () => {
   return (
-    <div>Hamkorlik</div>
+    <div className='hamkorlik'>
+      <section>
+        <h1>Hamkorlik</h1>
+        <p>Xitoylik mutaxassislar bilan hamkorlikda tomchilatib sug'orish texnologiyasi asosida yetishtirilgan paxtaning xosildorligi urganildi.</p>
+      </section>
+      <section></section>
+    </div>
   )
 }
 
