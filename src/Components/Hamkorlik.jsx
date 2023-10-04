003c578fd4be31a773db97d9ff3ed49bf34048b8
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-04 10:57:56 +0300

    structure is done

--- src/Components/Hamkorlik.jsx
+++ src/Components/Hamkorlik.jsx
@@ -7,7 +7,7 @@ const Hamkorlik = () => {
         <h1>Hamkorlik</h1>
         <p>Xitoylik mutaxassislar bilan hamkorlikda tomchilatib sug'orish texnologiyasi asosida yetishtirilgan paxtaning xosildorligi urganildi.</p>
         <p>Koson tumani Guvalak xududidagi "Kelajak oq oltin" fermer xujaligining paxta maydoniga "O'zbekiston-Xitoy paxtani tomchilatib sug'orish sinovi va texnologiyasini kengaytirish" loyixasi doirasida 2021 yilda Xitoy davlatida chigit olib kelib qadalgan edi.</p>
-        
+        <p>Ushbu loyixa Xitoy fanlar akademiyasining Shinjon ekologiya va geografiya insituti, Xitoy fanlar akademiyasi Geografik fanlar va tabiiy resurslarini tadqiq qilish insituti, O'zbekistan Respublikasi Suv hujaligi vazirligi, Irrigatsiya va suv muammolari ilmiy-tadqiqot insituti, Xinjiang Insitute of Ecology and Geography, Chinece academy of Sciences (XIEG) va Genetika va usimliklar eksperimental biologiyasi insituti, Amu-Qashqadaryo itxb hamkorlikda amalga oshirilgan edi.</p>
       </section>
       <section></section>
     </div>
