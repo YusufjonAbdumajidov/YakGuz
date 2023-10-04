8713dc0a1c8a7e405c02cb977c19589627c88a46
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-04 10:52:42 +0300

    new text eddad

--- src/Components/Hamkorlik.jsx
+++ src/Components/Hamkorlik.jsx
@@ -6,6 +6,8 @@ const Hamkorlik = () => {
       <section>
         <h1>Hamkorlik</h1>
         <p>Xitoylik mutaxassislar bilan hamkorlikda tomchilatib sug'orish texnologiyasi asosida yetishtirilgan paxtaning xosildorligi urganildi.</p>
+        <p>Koson tumani Guvalak xududidagi "Kelajak oq oltin" fermer xujaligining paxta maydoniga "O'zbekiston-Xitoy paxtani tomchilatib sug'orish sinovi va texnologiyasini kengaytirish" loyixasi doirasida 2021 yilda Xitoy davlatida chigit olib kelib qadalgan edi.</p>
+        
       </section>
       <section></section>
     </div>
