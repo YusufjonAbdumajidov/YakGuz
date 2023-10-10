6ceb385b1d7d4cf62efcb4afedd208ec1f1b4417
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-10 20:54:03 +0300

    new table created

--- src/Components/Qabul.jsx
+++ src/Components/Qabul.jsx
@@ -2,7 +2,52 @@ import React from 'react'
 
 const Qabul = () => {
   return (
-    <div>Qabul</div>
+    <div className='qabulContainer'>
+      <table border="1">
+        <tr>
+            <th>Header 1</th>
+            <th>Header 2</th>
+            <th>Header 3</th>
+            <th>Header 4</th>
+            <th>Header 5</th>
+        </tr>
+        <tr>
+            <td>Row 1, Column 1</td>
+            <td>Row 1, Column 2</td>
+            <td>Row 1, Column 3</td>
+            <td>Row 1, Column 4</td>
+            <td>Row 1, Column 5</td>
+        </tr>
+        <tr>
+            <td>Row 2, Column 1</td>
+            <td>Row 2, Column 2</td>
+            <td>Row 2, Column 3</td>
+            <td>Row 2, Column 4</td>
+            <td>Row 2, Column 5</td>
+        </tr>
+        <tr>
+            <td>Row 3, Column 1</td>
+            <td>Row 3, Column 2</td>
+            <td>Row 3, Column 3</td>
+            <td>Row 3, Column 4</td>
+            <td>Row 3, Column 5</td>
+        </tr>
+        <tr>
+            <td>Row 4, Column 1</td>
+            <td>Row 4, Column 2</td>
+            <td>Row 4, Column 3</td>
+            <td>Row 4, Column 4</td>
+            <td>Row 4, Column 5</td>
+        </tr>
+        <tr>
+            <td>Row 5, Column 1</td>
+            <td>Row 5, Column 2</td>
+            <td>Row 5, Column 3</td>
+            <td>Row 5, Column 4</td>
+            <td>Row 5, Column 5</td>
+        </tr>
+    </table>
+    </div>
   )
 }
 
